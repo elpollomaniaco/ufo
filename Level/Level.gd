fba889b85f253aa15aa5f5ce9749a0ccf8189f52
elpollomaniaco <elpollomaniaco@gmail.com>
2021-09-08 10:53:59 +0200

    Close pause menu with pause button.

--- Level/Level.gd
+++ Level/Level.gd
@@ -29,5 +29,6 @@ func _on_SpawnTimer_timeout():
 
 func _pause_game():
 	$PauseMenu.show()
+	$PauseMenu.set_process(true)
 	$PauseMenu/Buttons/Resume.grab_focus()
 	get_tree().paused = true
--- Level/PauseMenu.gd
+++ Level/PauseMenu.gd
@@ -1,12 +1,25 @@
 extends Control
 
 
+func _ready():
+	set_process(false)
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_pause"):
+		_resume_game()
+
 
 func _on_Resume_pressed():
-	hide()
-	get_tree().paused = false
+	_resume_game()
 
 
 func _on_MainMenu_pressed():
 	get_tree().change_scene("res://MainMenu/MainMenu.tscn")
 	get_tree().paused = false
+
+
+func _resume_game():
+	hide()
+	get_tree().paused = false
+	set_process(false)
