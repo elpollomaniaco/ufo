e13a66af9d4b5f213b4d2e8a7d75381b064584df
elpollomaniaco <elpollomaniaco@gmail.com>
2021-09-09 10:54:32 +0200

    Add return to main menu after game over.

--- Level/GameOver.gd
+++ Level/GameOver.gd
@@ -1,5 +1,13 @@
 extends Control
 
+var _input_is_deactivated: bool = true
+
+func _process(delta):
+	if not _input_is_deactivated:
+		if Input.is_action_just_pressed("player_tractor_beam"):
+			get_tree().change_scene("res://MainMenu/MainMenu.tscn")
+			get_tree().paused = false
+
 
 func set_outcome(successful: bool, total_score: int, collectible_score: int, time_score: int, health_score):
 	if successful:
@@ -9,3 +17,10 @@ func set_outcome(successful: bool, total_score: int, collectible_score: int, tim
 	$Scores/TimeScore/Value.text = str(time_score)
 	$Scores/HealthScore/Value.text = str(health_score)
 	$Scores/TotalScore/Value.text = str(total_score)
+	
+	$InputTimeout.start()
+
+
+func _on_InputTimeout_timeout():
+	_input_is_deactivated = false
+	$Return.show()
