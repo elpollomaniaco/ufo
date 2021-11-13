b16af51e06fa3b8c7f4f24ae2a91a2f4da6bb4ca
elpollomaniaco <elpollomaniaco@gmail.com>
2021-11-13 14:57:31 +0100

    Tell controller that collectible vanished.

--- collectibles/collectible.gd
+++ collectibles/collectible.gd
@@ -3,11 +3,15 @@ extends RigidBody
 
 export var _points: int
 
+onready var _collectibles_controller = get_tree().get_root().get_node("Level/CollectiblesController")
+
 
 func destroy():
+	_collectibles_controller.collectible_vanished()
 	queue_free()
 
 
 func collect() -> int:
+	_collectibles_controller.collectible_vanished()
 	queue_free()
 	return _points
