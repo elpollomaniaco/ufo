98e29d01a667733782ef17840bea25798bcf528d
elpollomaniaco <elpollomaniaco@gmail.com>
2021-11-25 11:55:56 +0100

    Make orbs shrink with health loss.

--- ui/gui/health_display/health_display.gd
+++ ui/gui/health_display/health_display.gd
@@ -1,5 +1,41 @@
 extends Control
 
 
+export var _min_orb_scale: float
+
+var _current_orb: int
+
+onready var _bar = $Bar
+onready var _health_orbs: Array = $Orbs.get_children()
+onready var _health_per_orb: float = 100.0 / _health_orbs.size()
+
+
+func _ready():
+	_init_elements()
+
+
 func change_value(new_value: int):
-	$Bar.value = new_value
+	_change_bar_value(new_value)
+	_change_orb_size(new_value)
+
+
+func _init_elements():
+	_bar.max_value = _health_per_orb
+	_bar.value = _bar.max_value
+	for orb in _health_orbs:
+		orb.rect_scale = Vector2.ONE
+	_current_orb = _health_orbs.size()
+
+
+func _change_bar_value(new_value: int):
+	var value: float = fmod(new_value, _health_per_orb)
+	_bar.value = value
+
+
+func _change_orb_size(new_value: int):
+	var orb_number: int = (new_value / _health_per_orb) as int + 1
+	var scale: float = fmod(new_value, _health_per_orb) / _health_per_orb
+	_health_orbs[-orb_number].rect_scale = Vector2.ONE * max(scale, _min_orb_scale)
+	if orb_number < _current_orb:
+		_health_orbs[-_current_orb].hide()
+		_current_orb = orb_number
