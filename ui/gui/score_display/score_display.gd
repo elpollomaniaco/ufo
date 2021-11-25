0a0131c12a3492b4b2a72bb0c316088c3ce55b84
elpollomaniaco <elpollomaniaco@gmail.com>
2021-11-25 08:21:13 +0100

    Make score count up incrementally.

--- ui/gui/score_display/score_display.gd
+++ ui/gui/score_display/score_display.gd
@@ -1,5 +1,30 @@
 extends Control
 
 
+export var _min_step: int
+
+var _score: int = 0
+
+# Score shown in this frame.
+onready var _shown_score: float = _score
+
+
+func _process(delta):
+	if _shown_score < _score:
+		var difference = (_score - _shown_score)
+		# Limit the value downwards so that growth does not become infinitely slow.
+		# Take that, Zeno!
+		var step = max(difference, _min_step) * delta
+		# Because of _min_step shown score could become higher than actual score.
+		_shown_score = min(_shown_score + step, _score)
+		_show_value(_shown_score)
+
+
 func change_value(new_value: int):
-	$Value.text = str(new_value)
+	_score = new_value
+
+
+func _show_value(new_value: float):
+	# float is needed for calculation during _process().
+	var value = str(new_value as int)
+	$Value.text = value
