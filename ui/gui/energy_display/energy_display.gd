395f88995b869050c0f20bfec4006232e5b2ce6f
elpollomaniaco <elpollomaniaco@gmail.com>
2022-05-25 10:42:26 +0200

    Change indicators on energy to show shots left instead of reserve.

--- ui/gui/energy_display/energy_display.gd
+++ ui/gui/energy_display/energy_display.gd
@@ -1,17 +1,16 @@
 extends Control
 
 
-# Not yet secured against changes in player node!
+# Attention: both not yet secured against changes in player node!
 const MAX_ENERGY = 100.0
+const INDICATOR_ENERGY = 20.0 # Weapon energy usage
 
 export var _tween_time: float
 
 var _shown_value: float = 0.0
 
-onready var _reserve_indicators: Array = $Reserve.get_children()
+onready var _indicators: Array = $Indicators.get_children()
 onready var _energy_container: TextureProgress = $Vial
-# x + 1 because main container is also filled.
-onready var _energy_in_container: float = MAX_ENERGY / (_reserve_indicators.size() + 1)
 onready var _tween: Tween = $Tween
 
 
@@ -25,34 +24,22 @@ func _process(_delta):
 
 
 func change_value(new_value: float):
-	var container_number: int = (new_value / _energy_in_container) as int
-	var container_value: float = fmod(new_value, _energy_in_container)
-	_change_container_value(container_number, container_value)
-
-
-func _change_container_value(container_number: int, new_value: float):
-	# Prevent container to appear empty when energy is at max.
-	# mod -> 0.
-	if container_number > _reserve_indicators.size():
-		new_value = _energy_in_container
-	
+	# Change shown value
 	# warning-ignore:return_value_discarded
 	_tween.interpolate_property(self, "_shown_value", null, new_value, _tween_time)
 	if not _tween.is_active():
 		# warning-ignore:return_value_discarded
 		_tween.start()
-	
-	if container_number < _reserve_indicators.size():
-		_reserve_indicators[container_number].deactivate()
-	
-	# Reactivating successor important when regeneration fills container.
-	# Successor means reserve container which would be emptied next.
-	var successor_container_number: int = container_number - 1
-	if successor_container_number >= 0 and successor_container_number < _reserve_indicators.size():
-		_reserve_indicators[successor_container_number].activate()
+		
+	# Change indicator state
+	for idx in _indicators.size():
+		var indicator = _indicators[idx]
+		if new_value >= ((idx + 1) * INDICATOR_ENERGY):
+			indicator.activate()
+		else:
+			indicator.deactivate()
 
 
 func _init_display():
-	_energy_container.max_value = _energy_in_container
-	for reserve_indicator in _reserve_indicators:
-		reserve_indicator.activate()
+	for indicator in _indicators:
+		indicator.activate()
