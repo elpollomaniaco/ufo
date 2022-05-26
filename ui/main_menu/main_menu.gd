b33c464e20ac949112ab7f7b7cfabeb5ded4918f
elpollomaniaco <elpollomaniaco@gmail.com>
2022-05-26 21:19:38 +0200

    Add background music change to menu and game.

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -1,8 +1,12 @@
 extends Control
 
 
+export var _background_music: AudioStream
+
+
 func _ready():
 	_init_buttons()
+	_set_background_music()
 
 
 func _on_start_pressed():
@@ -19,3 +23,7 @@ func _init_buttons():
 	$Buttons/AnimationPlayer.play("buttons_slide_in")
 	yield($Buttons/AnimationPlayer, "animation_finished")
 	$Buttons/Start.grab_focus()
+
+
+func _set_background_music():
+	AudioController.change_background_music(_background_music)
