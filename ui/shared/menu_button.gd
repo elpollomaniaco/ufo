af9db059fa641048a59a26d61266704fafa3fd38
elpollomaniaco <elpollomaniaco@gmail.com>
2022-06-01 10:19:45 +0200

    Add sound effect being played on pressed and selection.

--- ui/shared/menu_button.gd
+++ ui/shared/menu_button.gd
@@ -4,3 +4,11 @@ extends TextureButton
 # Make hover event trigger focus event to prevent double focus.
 func _on_mouse_entered():
 	self.grab_focus()
+
+
+func _on_focus_entered():
+	$SFX/Selected.play()
+
+
+func _on_pressed():
+	$SFX/Pressed.play()
