ac439d3ff6a71d65c2f00aabeaf4243ab9835fde
elpollomaniaco <elpollomaniaco@gmail.com>
2022-06-09 09:59:12 +0200

    Mask level change with slide in and out of buttons.

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -17,26 +17,17 @@ func _ready():
 
 
 func _on_start_pressed():
-	_change_level()
-
+	$Buttons/AnimationPlayer.play("buttons_slide_out")
+	yield($Buttons/AnimationPlayer, "animation_finished")
+	yield(get_tree(),"idle_frame") # Change starts a little bit to early.
+	SceneController.change_scene(_level_resource)
 
 func _on_exit_pressed():
 	get_tree().quit()
 
 
-func _show_buttons():
-	yield(get_tree().create_timer(0.5), "timeout")
-	$Buttons/AnimationPlayer.play("buttons_slide_in")
-	yield($Buttons/AnimationPlayer, "animation_finished")
-	$Buttons/Start.grab_focus()
-
-
 func _set_background_music():
 	AudioController.change_background_music(_background_music)
-
-
-func _change_level():
-	SceneController.change_scene(_level_resource)
 
 
 func _on_loading_progress_changed(progress: float):
@@ -46,4 +37,6 @@ func _on_loading_progress_changed(progress: float):
 func _on_scene_loaded(level_resource: Resource):
 	_level_resource = level_resource
 	$Loading.hide()
-	_show_buttons()
+	$Buttons/AnimationPlayer.play("buttons_slide_in")
+	yield($Buttons/AnimationPlayer, "animation_finished")
+	$Buttons/Start.grab_focus()
